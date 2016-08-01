81868577bfef1e3446f32eb213f002a97df75bf3
Randall Rauwendaal <randallr@apple.com>
2016-08-01 15:11:10 -0700

    The application is generally improved

--- texture/Texture2D_Multi.h
+++ texture/Texture2D_Multi.h
@@ -21,13 +21,18 @@ class Texture2D_Multi : public TextureT<tex2D_Multi, TexType, IntFormat, PixForm
 public:
 	GLuint w, h;
 	GLuint samples;
+	GLboolean fixedsamplelocations;
 
 	GLuint wrap_s;
 	GLuint wrap_t;
 
 public:
-	//Constructors
+	// Constructors
 	Texture2D_Multi(GLuint w, GLuint h, GLuint numSamples = 1, bool fixed = false);
+
+	// Set Samples
+	void setSamples(GLuint numSamples);
+
 
 };
 //End class Texture
@@ -41,6 +46,7 @@ Texture2D_Multi<TexType, IntFormat, PixFormat>::Texture2D_Multi(GLuint w, GLuint
 	this->w = w;
 	this->h = h;
 	this->samples = numSamples;
+	this->fixedsamplelocations = fixed;
 
 	wrap_s = GL_CLAMP_TO_BORDER;
 	wrap_t = GL_CLAMP_TO_BORDER;
@@ -58,6 +64,21 @@ Texture2D_Multi<TexType, IntFormat, PixFormat>::Texture2D_Multi(GLuint w, GLuint
 								w,
 								h,
 								fixed);
+	}
+	this->unbind();
+}
+
+template<class TexType, class IntFormat, class PixFormat>
+void Texture2D_Multi<TexType, IntFormat, PixFormat>::setSamples(GLuint numSamples)
+{
+	this->bind();
+	{
+		glTexImage2DMultisample(traits<tex2D_Multi>::target,
+								numSamples,
+								traits<IntFormat>::internal_format,
+								w,
+								h,
+								fixedsamplelocations);
 	}
 	this->unbind();
 }
